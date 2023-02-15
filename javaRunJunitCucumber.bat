e56939e38116e185b2c9c8cc5f76a9e9d3ad71cd
cynthiachowdhury0 <97745949+cynthiachowdhury0@users.noreply.github.com>
2023-02-15 11:46:27 -0500

    Update javaRunJunitCucumber.bat

--- javaRunJunitCucumber.bat
+++ javaRunJunitCucumber.bat
@@ -1,1 +1,1 @@
-java -Dfile.encoding=utf-8 -cp D:/Demo/cucumber-sample/resources/libs/*;D:/Demo/cucumber-sample/target/*; org.junit.runner.JUnitCore "com.glue.RunTest"
+java -Dfile.encoding=utf-8 -cp D:/Demo/cucumber-sample/resources/libs/*;D:/Demo/cucumber-sample/target/*; org.junit.runner.RunWith "com.glue.RunTest"
